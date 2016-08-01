da100589adca81660ec163bfe2aee1576130ff07
Randall Fine <randallfine@gmail.com>
2016-08-01 05:04:50 +0000

    updates

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 backend projects for Udemy web dev bootcamp
 mostly using Node, Express, EJS, Mongo/Mongoose and NPM.
 Working with JSON data.
+
+mongod --bind_ip=$IP --nojournal
--- RESTfulRouting/RESTfulBlogApp/app.js
+++ RESTfulRouting/RESTfulBlogApp/app.js
@@ -1,4 +1,5 @@
 var bodyParser = require("body-parser"),
+methodOverride = require("method-override"),
 mongoose       = require("mongoose"),
 express        = require("express"),
 app            = express();
@@ -8,6 +9,7 @@ mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 // MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
@@ -48,20 +50,52 @@ app.get("/blogs/new", function(req, res){
 
 //CREATE ROUTE
 app.post("/blogs", function(req, res){
-    //create blog
+    // create blog
     Blog.create(req.body.blog, function(err, newBlog){
         if(err){
             res.render("new");
         } else {
-            //redirect to index
+            //then, redirect to the index
             res.redirect("/blogs");
+            
         }
     });
 });
 
 //SHOW ROUTE
 app.get("/blogs/:id", function(req, res) {
-    res.send("SHOW PAGE");
+   Blog.findById(req.params.id, function(err, foundBlog){
+       if(err){
+          res.redirect("/blogs");
+          console.log(err);
+       } else {
+           res.render("show", {blog: foundBlog});
+       }
+   });
+});
+
+//EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            console.log(err);
+            res.redirect("/blogs")
+        } else {
+            res.render("edit", {blog: foundBlog});
+        }
+    });
+});
+
+//UPDATE ROUTE
+app.put("/blogs/:id", function(req, res){
+   Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+       if(err){
+           console.log(err);
+           res.redirect("/blogs")
+       }else{
+           res.redirect("/blogs/" + req.params.id);
+       }
+   });
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
