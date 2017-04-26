6d2f6007e2df117feb1b6d5333044722f7820baa
Randall Fine <randallfine@gmail.com>
2017-04-26 15:30:11 +0000

    starting over cleaning up files

--- IntroToNode/objectives.md
+++
@@ -1,4 +0,0 @@
-#Using Node
-
-*Interact with Node console
-*Run a file in Node
--- YelpCamp/v7/models/comments.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require("mongoose");
-
-// SCHEMA SETUP
-var commentSchema = new mongoose.Schema({
-   text: String,
-   author: String
-});
-
-module.exports = mongoose.model("Comment", commentSchema);
